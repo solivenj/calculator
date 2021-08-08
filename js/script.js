7f1ecc3f1f3ad802bb3e5c0beb782b0050a08d15
solivenj <soliven.j@husky.neu.edu>
2021-08-07 20:51:52 -0400

    Input Refactor

--- js/script.js
+++ js/script.js
@@ -8,16 +8,22 @@ const negativeButton = buttons.querySelector("#negative");
 const operators = buttons.querySelectorAll(".operator");
 const equals = document.getElementById("equals");
 
-let operation = "";
+let firstNum = undefined;
+let firstOperation = undefined;
+let operation = undefined;
+let secondNum = undefined;
+// let secondOperation = undefined;
+let getNextNum = undefined;
 
 const numbersList = [...numbers];
 numbersList.push(negativeButton)
 
 numbersList.forEach(number => number.addEventListener("click", enterInput));
 operators.forEach(operator => operator.addEventListener("click", handleOperator));
+equals.addEventListener('click', handleEquals);
 cButton.addEventListener("click", deleteInput);
 
-acButton.onclick = () => currentText.textContent = "0";
+acButton.addEventListener('click', clearCalc);
 
 
 const add = (num1, num2) => num1 + num2;
@@ -37,7 +43,8 @@ function operate(num1, operator, num2) {
             result = subtract(num1, num2);
             break;
         case 'x':
-            returnresult = multiply(num1, num2);
+            result = multiply(num1, num2);
+            break;
         case '÷':
             result = divide(num1, num2);
             break;
@@ -49,75 +56,16 @@ function operate(num1, operator, num2) {
     return result;
 }
 
-function enterInput() {
-    let value = this.textContent;
-    let fontSize = "";
-    
-    if (currentText.textContent == "0") {
-        if (value == "+/-") {
-            if (currentText.textContent[0] == "-") {
-                currentText.textContent = currentText.textContent.slice(1);
-            } else {
-                currentText.textContent = "-" + currentText.textContent;
-            }
-        } else {
-            currentText.textContent = value;
-        }
-    } else if (currentText.textContent.length < 9) {
-        if (value == "+/-") {
-            if (currentText.textContent[0] == "-") {
-                currentText.textContent = currentText.textContent.slice(1);
-            } else {
-                currentText.textContent = "-" + currentText.textContent;
-            }
-        } else {
-            if (currentText.textContent.length < 9) {
-                currentText.textContent += value;
-            }
-        }
-
-        if (currentText.textContent.length <= 6) {
-            fontSize = "5em";
-        } else if (currentText.textContent.length <= 7) {
-            fontSize = "4.25em";
-        } else if (currentText.textContent.length <= 8) {
-            fontSize = "4em";
-        } else if (currentText.textContent.length <= 9) {
-            fontSize = "3.75em";
-        }
-        currentText.style.fontSize = fontSize;
+function handleNegative() {
+    if (currentText.textContent[0] == "-") {
+        currentText.textContent = currentText.textContent.slice(1);
     } else {
-        if (value == "+/-") {
-            if (currentText.textContent[0] == "-") {
-                currentText.textContent = currentText.textContent.slice(1);
-            } else {
-                currentText.textContent = "-" + currentText.textContent;
-            }
-        }
-
-        if (currentText.textContent.length <= 6) {
-            fontSize = "5em";
-        } else if (currentText.textContent.length <= 7) {
-            fontSize = "4.25em";
-        } else if (currentText.textContent.length <= 8) {
-            fontSize = "4em";
-        } else if (currentText.textContent.length <= 9) {
-            fontSize = "3.75em";
-        } else {
-            fontSize = "3.5em";
-        }
-
-        currentText.style.fontSize = fontSize;
+        currentText.textContent = "-" + currentText.textContent;
     }
 }
 
-function deleteInput() {
-    if (currentText.textContent.length) {
-        currentText.textContent = currentText.textContent.slice(0, -1);
-        if (!currentText.textContent.length) {
-            currentText.textContent = "0";
-        }
-    }
+function setFontSize() {
+    let fontSize = "";
 
     if (currentText.textContent.length <= 6) {
         fontSize = "5em";
@@ -128,36 +76,119 @@ function deleteInput() {
     } else if (currentText.textContent.length <= 9) {
         fontSize = "3.75em";
     }
-    currentText.style.fontSize = fontSize;
 
-    operation.textContent = "";
+    currentText.style.fontSize = fontSize;
+}
+
+function enterInput() {
+    let value = this.textContent;
+    if (!operation) {
+        if (currentText.textContent == "0") {
+            value == "+/-" ? handleNegative() : currentText.textContent = value;
+        } else if (currentText.textContent == "-0") {
+            value == "+/-" ? handleNegative() : currentText.textContent = currentText.textContent[0] + value;
+        } else if (currentText.textContent.length < 9) {
+            value == "+/-" ? handleNegative() : currentText.textContent += value;
+            setFontSize();
+        } else {
+            if (value == "+/-") handleNegative();
+            currentText.textContent.length <= 9 ? setFontSize() : currentText.style.fontSize = "3.5em";
+        }
+    } else {
+        if (currentText.textContent == firstNum) {
+            currentText.textContent = "0";
+            value == "+/-" ? handleNegative() : currentText.textContent = value;
+        } else if (currentText.textContent.length < 9) {
+            value == "+/-" ? handleNegative() : currentText.textContent += value;
+            setFontSize();
+        } else {
+            if (value == "+/-") handleNegative();
+            currentText.textContent.length <= 9 ? setFontSize() : currentText.style.fontSize = "3.5em";
+        }
+    }
+}
+
+function deleteInput() {
+    if (currentText.textContent.length) {
+        currentText.textContent = currentText.textContent.slice(0, -1);
+        if (currentText.textContent == "-") {
+            currentText.textContent += "0";
+        } else if (!currentText.textContent.length) {
+            currentText.textContent = "0";
+        }
+    }
+
+    setFontSize();
+    // firstOperation.textContent = "";
 }
 
 
 function handleOperator() {
-    // console.log(this.textContent);
-    if (!operation.length) {
-        operation = currentText.textContent + ` ${this.textContent} `;
+    if (!firstNum) {
+        // currentText is first num
+        firstNum = currentText.textContent;
+        operation = this.textContent;
         this.classList.add("active");
-        currentText.textContent = "0";
-        currentText.style.fontSize = "5em";
-        console.log(operation);
-    } else {
-        operation += currentText.textContent;
-        console.log(operation);
-        fullOperation = operation.split(' ');
-        console.log(fullOperation)
-        console.log(operate(...fullOperation));
-        currentText.textContent = operate(...fullOperation);
+        
+
+    } else if (!secondNum) {
+        secondNum = currentText.textContent;
+        if (operation) {
+            // ready to operate
+            // secondNum = currentText.textContent;
+            result = operate(firstNum, operation, secondNum);
+            console.log(result);
+            currentText.textContent = result;
+
+            firstNum = result;
+            
+            if (this.textContent != "=") {
+                removeActiveButton();
+                this.classList.add("active");
+                operation = this.textContent;
+            } else {
+                removeActiveButton();
+                operation = undefined;
+            }
+            
+            secondNum = undefined;
+        } else {
+            operation = this.textContent;
+            if (this.textContent != "=") {
+                removeActiveButton();
+                this.classList.add("active");
+            }
+        }
     }
 }
 
+function removeActiveButton() {
+    let active = document.querySelector(".active"); // shoud only be one active
+    if (active) active.classList.remove("active");
+}
 
+function clearCalc() {
+    currentText.textContent = "0";
+    firstNum = undefined;
+    firstOperation = undefined;
+    secondNum = undefined;
+    secondOperation = undefined;
+    
+    let active = document.querySelector(".active");
+    if (active) active.classList.remove("active");
+}
 
+function handleEquals() {
+    if (operation) {
+        secondNum = currentText.textContent;
+        result = operate(firstNum, operation, secondNum);
+        console.log(result);
+        currentText.textContent = result;
+        setFontSize();
 
-
-
-
-
-
+        removeActiveButton();
+        operation = undefined;
+        secondNum = undefined;
+    }
+}
 
